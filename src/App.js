c0504c349fbedd998c4e4dc45c0cf06a7ca918d9
Petrov Michil Ivanovich <fitim1501@gmail.com>
2023-03-29 17:04:29 +0900

    select size and types

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
 function App() {
   return (
     <div className="wrapper">
@@ -17,8 +18,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={500} />
-            <PizzaBlock title="Pepperoni" price={800} />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,16 +4,12 @@ function Categories() {
   const [activeIndex, setActiveIndex] = React.useState(0);
 
   const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
-  const onClickCategories = (ind) => {
-    setActiveIndex(ind);
-  };
-  console.log(activeIndex);
   return (
     <div className="categories">
       <ul>
         {
           categories.map((value, i) =>
-            <li onClick={()=> onClickCategories(i)} className={activeIndex === i? "active": ''}>
+            <li onClick={()=> setActiveIndex(i)} className={activeIndex === i? "active": ''}>
               {value}
               </li>
           )
