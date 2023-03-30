248d676fdee93acbcf428f9681f0f8e009f20670
Petrov Michil Ivanovich <fitim1501@gmail.com>
2023-03-30 10:49:33 +0900

    add useEffect

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
+let fetched = false;
+
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6424e00e7ac292e3cff2bc29.mockapi.io/items')
+      .then((res) => res.json())
+      .then((res) => setItems(res));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
