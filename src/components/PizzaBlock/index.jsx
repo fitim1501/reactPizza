00722ae41e567c44606221bf0bc8b271d7963b36
Petrov Michil Ivanovich <fitim1501@gmail.com>
2023-04-03 11:35:04 +0900

    scss

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -6,7 +6,8 @@ function PizzaBlock({title, price, imageUrl,sizes,types}) {
   const typeNames = ['тонкое', 'традиционное'];
 
   return(
-  <div className="pizza-block">
+  <div className="pizza-block-wrapper">
+    <div className="pizza-block">
   <img
     className="pizza-block__image"
     src={imageUrl}
@@ -49,6 +50,7 @@ function PizzaBlock({title, price, imageUrl,sizes,types}) {
     </button>
   </div>
 </div>
+  </div>
 );
 }
 
